e2fe6aa60ed8ad2791235b0de58cd045bd443168
jesse soldat <jsoldat@hotmail.com>
2016-08-13 11:46:54 +0700

    can connect to local database

--- app.js
+++ app.js
@@ -9,7 +9,30 @@ var http = require('http'),
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'jade');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static(__dirname + '/app/public'));
+//MongoDB
+var dbHost = process.env.DB_HOST || 'localhost';
+var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'node-login';
+var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+console.log(dbURL);
 
+
+app.use(session({
+	secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+	proxy: true,
+	resave: true,
+	saveUninitialized: true,
+	store: new MongoStore({ url: dbURL })
+	})
+);
 
 
 http.createServer(app).listen(app.get('port'), function(){
